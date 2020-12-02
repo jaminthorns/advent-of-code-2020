f4b38cfdf5ffa38c2c2337a09b2eb589f6a3efb7
Jamin Thornsberry <jamin.thorns@gmail.com>
2020-12-02 01:29:16 -0600

    Day 1

---
+++ lib/day_1.ex
@@ -0,0 +1,56 @@
+defmodule Day1 do
+  @behaviour Solution
+
+  @doc ~S"""
+  iex> solve_part_1("1721
+  ...> 979
+  ...> 366
+  ...> 299
+  ...> 675
+  ...> 1456")
+  514579
+  """
+  def solve_part_1(input) do
+    input
+    |> numbers()
+    |> find_product_by_sum(2020, 2)
+  end
+
+  @doc ~S"""
+  iex> solve_part_2("1721
+  ...> 979
+  ...> 366
+  ...> 299
+  ...> 675
+  ...> 1456")
+  241861950
+  """
+  def solve_part_2(input) do
+    input
+    |> numbers()
+    |> find_product_by_sum(2020, 3)
+  end
+
+  defp numbers(input) do
+    input
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp find_product_by_sum(numbers, sum, count, locked \\ [])
+
+  defp find_product_by_sum(_, sum, 0, locked) do
+    if Enum.sum(locked) == sum do
+      Enum.reduce(locked, &Kernel.*/2)
+    end
+  end
+
+  defp find_product_by_sum(numbers, sum, count, locked) do
+    numbers
+    |> Enum.reduce({[], numbers}, fn _, {seqs, seq} -> {[seq | seqs], tl(seq)} end)
+    |> elem(0)
+    |> Enum.find_value(fn [number | rest] ->
+      find_product_by_sum(rest, sum, count - 1, [number | locked])
+    end)
+  end
+end
--- test/all_days_test.exs
+++ test/all_days_test.exs
@@ -1,3 +1,5 @@
 defmodule AllDaysTest do
   use ExUnit.Case
+
+  doctest Day1, import: true, tags: :day_1
 end
