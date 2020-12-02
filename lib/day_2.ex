b187bc3cb019fbc60668c67ba4fb8d319df95227
Jamin Thornsberry <jamin.thorns@gmail.com>
2020-12-02 02:01:46 -0600

    Day 2

---
+++ lib/day_2.ex
@@ -0,0 +1,59 @@
+defmodule Day2 do
+  @behaviour Solution
+
+  @password_pattern ~r/(?<num_a>\d+)-(?<num_b>\d+) (?<letter>[a-z]): (?<password>\w+)/
+
+  @doc """
+  iex> solve_part_1("1-3 a: abcde
+  ...> 1-3 b: cdefg
+  ...> 2-9 c: ccccccccc")
+  2
+  """
+  def solve_part_1(input) do
+    input
+    |> passwords()
+    |> Enum.count(&password_valid_range?/1)
+  end
+
+  @doc """
+  iex> solve_part_2("1-3 a: abcde
+  ...> 1-3 b: cdefg
+  ...> 2-9 c: ccccccccc")
+  1
+  """
+  def solve_part_2(input) do
+    input
+    |> passwords()
+    |> Enum.count(&password_valid_positions?/1)
+  end
+
+  defp passwords(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&Regex.named_captures(@password_pattern, &1))
+    |> Enum.map(fn entry ->
+      %{
+        num_a: String.to_integer(entry["num_a"]),
+        num_b: String.to_integer(entry["num_b"]),
+        letter: entry["letter"],
+        password: entry["password"]
+      }
+    end)
+  end
+
+  defp password_valid_range?(entry) do
+    count =
+      entry.password
+      |> String.graphemes()
+      |> Enum.count(&(&1 == entry.letter))
+
+    count in entry.num_a..entry.num_b
+  end
+
+  defp password_valid_positions?(entry) do
+    letter_a = String.at(entry.password, entry.num_a - 1)
+    letter_b = String.at(entry.password, entry.num_b - 1)
+
+    entry.letter in [letter_a, letter_b] and letter_a != letter_b
+  end
+end
--- test/all_days_test.exs
+++ test/all_days_test.exs
@@ -2,4 +2,5 @@ defmodule AllDaysTest do
   use ExUnit.Case
 
   doctest Day1, import: true, tags: :day_1
+  doctest Day2, import: true, tags: :day_2
 end
