8ff1e9b71afcaf97ffc0eefaa98c04910e0e6b35
Jamin Thornsberry <jamin.thorns@gmail.com>
2020-12-07 18:11:30 -0600

    Move `solve` function to `Solution` and remove `Solver`

--- lib/solution.ex
+++ lib/solution.ex
@@ -1,4 +1,17 @@
 defmodule Solution do
   @callback solve_part_1(binary) :: any
   @callback solve_part_2(binary) :: any
+
+  @doc """
+  Solve a puzzle for a given `day` and `part`.
+  """
+  def solve(day, part) do
+    input = File.read!("input/day_#{day}.txt")
+    module = String.to_atom("Elixir.Day#{day}")
+
+    case part do
+      1 -> module.solve_part_1(input)
+      2 -> module.solve_part_2(input)
+    end
+  end
 end
